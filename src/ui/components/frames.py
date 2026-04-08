57dda092f9e4c09f601db5bf553005ac6b023f28
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-04-08 15:56:37 -0300

    feat: Layout da lista de pacientes aprimorado com funcionalidade de busca e inclusão de fichas de pacientes.

--- src/ui/components/frames.py
+++ src/ui/components/frames.py
@@ -40,19 +40,25 @@ class FrameListaPacientes(ctk.CTkFrame):
         super().__init__(master, **kwargs)
 
         self.selecionar_callback = selecionar_callback
+        
+        frame_input = ctk.CTkFrame(self, fg_color="#f0f0f0", corner_radius=10)
 
-        ctk.CTkLabel(self, text="Pacientes",
-                     font=("Arial", 20, "bold")).pack(pady=10)
+        ctk.CTkLabel(self, text="Pacientes", font=("Arial", 22, "bold"),
+                     text_color="#080808").pack(pady=10)
 
-        self.busca = ctk.CTkEntry(self, placeholder_text="Buscar por nome ou telefone")
-        self.busca.pack(pady=5)
+        self.busca = ctk.CTkEntry(frame_input, placeholder_text="Buscar por nome ou telefone")
+        self.busca.pack(pady=10, padx=10, fill="both", expand=True, side='left')
 
-        ctk.CTkButton(self, text="Buscar", command=self.listar).pack(pady=5)
+        ctk.CTkButton(frame_input, text="Buscar", height=40,command=self.listar).pack(pady=10, padx=10, side='left')
 
-        self.frame_lista = ctk.CTkFrame(self)
+        self.frame_lista = ctk.CTkScrollableFrame(self, fg_color="#f0f0f0", corner_radius=10)
+        
+        self.listar()
+
+        frame_input.pack(padx=20, fill="x")
         self.frame_lista.pack(fill="both", expand=True, pady=10)
 
-        self.listar()
+        
 
     def listar(self):
         for widget in self.frame_lista.winfo_children():
@@ -63,12 +69,10 @@ class FrameListaPacientes(ctk.CTkFrame):
 
         for p in pacientes:
             if termo in p["nome"].lower() or termo in p["telefone"]:
-                btn = ctk.CTkButton(
-                    self.frame_lista,
-                    text=f"{p['nome']} - {p['telefone']}",
-                    command=lambda paciente=p: self.selecionar(paciente)
-                )
-                btn.pack(pady=2, fill="x")
+                card = FrameCardPacientes(self.frame_lista, nome=p["nome"], data_nascimento=p["data_nascimento"],
+                                            telefone=p["telefone"], email=p["email"], corner_radius=10)
+                card.pack(pady=5, padx=20, fill="x")
+            
 
     def selecionar(self, paciente):
         if self.selecionar_callback:
@@ -200,5 +204,26 @@ class FrameCard(ctk.CTkFrame):
         
         self.valor.pack(padx=10, pady=(10, 0), anchor='w')
         self.descricao.pack(padx=10, pady=(0, 10), anchor='w')
+
+class FrameCardPacientes(ctk.CTkFrame):
+    def __init__(self, master, nome, data_nascimento, telefone, email, **kwargs):
+        super().__init__(master, **kwargs)
+
+        data = data_nascimento.split("-")
+        data_nascimento = f"{data[2]}/{data[1]}/{data[0]}"
+
+        self.nome = ctk.CTkLabel(self, text=nome, font=("D-DIN-Bold", 18, "bold"),
+                     text_color="#f0f0f0")
+        self.data_nascimento = ctk.CTkLabel(self, text=f"Nascimento: {data_nascimento}", font=("D-DIN-Bold", 14),
+                     text_color="#f0f0f0")
+        self.telefone = ctk.CTkLabel(self, text=f"Telefone: {telefone}", font=("D-DIN-Bold", 14),
+                     text_color="#f0f0f0")
+        self.email = ctk.CTkLabel(self, text=f"E-mail: {email}", font=("D-DIN-Bold", 14),
+                     text_color="#f0f0f0")
+        
+        self.nome.pack(padx=10, pady=(10, 0), anchor='w')
+        self.data_nascimento.pack(padx=10, pady=(0, 0), anchor='w')
+        self.telefone.pack(padx=10, pady=(0, 0), anchor='w')
+        self.email.pack(padx=10, pady=(0, 10), anchor='w')
 
   
